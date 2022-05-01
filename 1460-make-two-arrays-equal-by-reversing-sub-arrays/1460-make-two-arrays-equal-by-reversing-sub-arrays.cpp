268b9ba562b7a4a3af1cdceea878cf0d62bfe866
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2022-05-01 16:19:31 +0530

    Time: 62 ms (5.03%), Space: 15.9 MB (19.95%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cpp
@@ -3,15 +3,31 @@ public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
         
         unordered_map<int, int> umap1;
-        unordered_map<int, int> umap2;
+        // unordered_map<int, int> umap2;
         
         for(int i = 0; i< target.size(); ++i){
-            umap1[arr[i]]++;
-            umap2[target[i]]++;
+            umap1[target[i]]++;
+            // umap2[target[i]]++;
         }
         
-        if(umap1 == umap2) return true;
-        return false;
+        // if(umap1 == umap2) return true;
+        // return false;
         
+        for(auto it: arr){
+            umap1[it]--;
+            if(umap1[it] == 0){
+                umap1.erase(it);
+            }
+        }
+        
+        for(auto it: umap1){
+            cout << it.first << " " << it.second << endl;
+        }
+        
+        //cout << umap1.size() << endl;
+        if(umap1.size() == 0) return true;
+        
+        
+        return false;;
     }
 };
