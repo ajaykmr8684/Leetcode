79d88bf5ab014a2136e7f9f3bed2fe6d8f661e18
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2025-02-24 18:32:34 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        vector<int> map1(26, 0);
-        vector<int> map2(26, 0);
+        vector<int> map(26, 0);
 
         for(int i = 0; i < s.length(); ++i) {
-            map1[s[i] - 'a']++;
-            map2[t[i] - 'a']++; 
+            map[s[i] - 'a']++;
+            map[t[i] - 'a']--; 
         }
 
         for(int i = 0; i < 26; ++i) {
-            if(map1[i] != map2[i]) return false;
+            if(map[i] > 0) return false;
         }
 
         return true;
