c66c647bf9a5c94df321043b15fa84213ab27a4c
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2025-02-24 18:58:28 +0530

    Time: 35 ms (44.01%), Space: 73.6 MB (50.71%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> umap;
+
+        for(auto it: nums)
+        {
+            umap[it]++;
+            if(umap[it] > 1){
+                return true;
+            }
+            
+        }    
+
+        return false;    
+    }
+};
