ab42a35cd7625372c15d9dbe707f66e164dc8106
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2025-03-13 15:22:46 +0530

    Time: 0 ms (100%), Space: 19.6 MB (99.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> mp;
-        vector<int> ans;
+        int candidate1 =NULL;
+        int count1 = 0;
+        int candidate2 = NULL;
+        int count2 = 0;
 
-        for(auto it: nums) {
-            mp[it]++;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(candidate1 == nums[i]) {
+                count1++;
+            }
+            else if(candidate2 == nums[i]) {
+                count2++;
+            }
+            else if(count1 == 0) {
+                candidate1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0) {
+                candidate2 = nums[i];
+                count2 = 1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
         }
 
-        for(auto it: mp) {
-            if(it.second>n/3 ) ans.push_back(it.first);
+        vector<int> result;
+        int freq1 = 0;
+        int freq2 = 0;
+
+        for(int &num: nums) {
+            if(num == candidate1) freq1++;
+            else if(num == candidate2) freq2++;
         }
 
-        return ans;
+        if(freq1 > floor(nums.size()/3)){
+            result.push_back(candidate1);
+        }
 
+        if(freq2 > floor(nums.size()/3)) {
+            result.push_back(candidate2);
+        }
 
+        return result;
     }
 };
