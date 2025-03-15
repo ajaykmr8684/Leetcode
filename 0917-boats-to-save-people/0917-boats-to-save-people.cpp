11258da8dd890e6de8fdc67e2ae5e90e727e4b80
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2025-03-15 18:16:50 +0530

    Time: 25 ms (7.97%), Space: 45.8 MB (68.71%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int i = 0;
+        int j = people.size() - 1;
+        int ans = 0;
+        while(i <= j) {
+            int sum = people[i] + people[j] ;
+            if(sum <= limit){
+                ans++;
+                i++;
+                j--;
+            }
+            else if(sum > limit) {
+                ans++;
+                j--;
+            }
+        }
+
+        return ans;
+
+        
+
+    }
+};
