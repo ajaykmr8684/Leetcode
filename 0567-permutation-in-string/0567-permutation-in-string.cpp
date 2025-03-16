54342dd642789593904fb376eef91b697498ff93
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2025-03-17 00:47:54 +0530

    Time: 2472 ms (5.01%), Space: 123.9 MB (5.03%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int left = 0;
-        int right = s1.length() - 1;
-
+        string original = s1;
+        sort(original.begin(), original.end());
         for(int i = 0; i < s2.length(); ++i) {
-            string temp = s2.substr(left, s1.length());
+            string temp = s2.substr(i, s1.length());
             sort(temp.begin(), temp.end());
-            string original = s1;
-            sort(original.begin(), original.end());
-
             if(temp == original) {
                 return true;
             }
-
-            left++;
         }
 
         return false;
