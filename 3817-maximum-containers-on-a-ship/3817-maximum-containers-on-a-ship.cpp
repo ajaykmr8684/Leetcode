8a8d5c671b210c36385af10e127a0cb2bff66809
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2025-03-23 13:13:34 +0530

    Time: 0 ms (100%), Space: 8.2 MB (97.8%) - LeetHub

--- 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.cpp
+++ 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int maxContainers(int n, int w, int maxWeight) {
-        int count = 0;
-
-        int actualDecks = n*n;
-
-        while(maxWeight - w >= 0 && actualDecks > 0) {
-            maxWeight = maxWeight - w;
-            actualDecks--;
-        }
-
-        cout << actualDecks;
-
-        return n*n - actualDecks;
+        return min(maxWeight/w, n*n);
     }
 };
