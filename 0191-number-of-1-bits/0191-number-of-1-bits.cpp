11d4295b85a30ac0289f759703377a4d3368d79d
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2025-04-19 01:40:33 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.77%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,6 +4,8 @@ public:
         int temp = n;
         int count = 0;
 
+
+        // Brian Kernighan’s Algorithm ( Just AND it with lower number and you will get rid of the precedence 1)
         while(temp) {
             temp = temp & temp-1;
             count++;
