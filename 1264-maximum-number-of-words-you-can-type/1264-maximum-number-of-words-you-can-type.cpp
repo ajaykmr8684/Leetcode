09d2ddcc6fde0a125eae15dcd32aa9f5ca42efaf
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2025-09-15 20:11:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (79.4%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int canBeTypedWords(string text, string brokenLetters) {
+    int canBeTypedWords(string text, string brokenLetters) { //"ad"
         unordered_set<char> st;
-        for(auto it: brokenLetters) st.insert(it);
         int ans = 0;
         bool canType = true;
-
-        for(int i = 0; i < text.length(); ++i) {
-            if(text[i] == ' ') {
-                if(canType) {
-                    ans++;
-                }
-                canType = true;
-            }
-            else if(st.find(text[i]) != st.end()) {
-                canType = false;
-            }
+        for(char it: brokenLetters) {
+            st.insert(it); 
         }
 
-        if(canType) ans++;
+            // ['a', 'd'];
 
+
+            for(int i = 0; i < text.length(); ++i) {
+                if(text[i] == ' ') {
+                    if(canType) {
+                        ans++;
+                    }
+                    canType = true;
+                }
+                else if(st.find(text[i]) != st.end()){
+                    canType = false;
+                }
+            }
+
+            if(canType) ans++;
         return ans;
+
     }
 };
